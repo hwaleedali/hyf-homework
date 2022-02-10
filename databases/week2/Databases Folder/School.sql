31428bc4ce60d0327e22f687a7eab7ca4a185d91
hwaleedali <hwaleedalibutt@gmail.com>
2022-02-10 16:44:50 +0500

    Database week2

---
+++ databases/week2/Databases Folder/School.sql
@@ -0,0 +1,37 @@
+use School;
+CREATE TABLE `Class` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(255) NOT NULL,
+  `begin` DATETIME NOT NULL,
+  `end` DATETIME NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+CREATE TABLE `Student` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `class_id` int(10) unsigned NOT NULL,
+  CONSTRAINT `fk_Class` FOREIGN KEY (`class_id`) REFERENCES `Class` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+alter table
+  `Class`
+add
+  `status` varchar(255) NOT NULL;
+INSERT INTO
+  Class (name, begin,end,
+  status
+)
+VALUES
+  (
+    "Class20",
+    "2020-02-03",
+    "2020-02-04",
+    "not-started"
+  ),
+  ("Class19", "2020-02-03", "2020-02-04", "Ongoing"),
+  (
+    "Class18",
+    "2020-02-03",
+    "2020-02-04",
+    "Finished"
+  );
