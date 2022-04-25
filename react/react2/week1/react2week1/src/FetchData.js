34f2e1c646f79c86fb3421da0972ef99e39af5cd
hwaleedali <hwaleedalibutt@gmail.com>
2022-04-25 16:44:29 +0200

    change code

--- react/react2/week1/react2week1/src/FetchData.js
+++ react/react2/week1/react2week1/src/FetchData.js
@@ -4,43 +4,46 @@ import { SearchContext } from "./GithubSearch";
 export function FetchData() {
   const UserContext = useContext(SearchContext);
   const [userfetch, setUserFetch] = useState([]);
-  const [errormessage, setErrorMessage] = useState([]);
+  const [errormessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  const API = `https://api.github.com/search/users?q=${UserContext}`;
   useEffect(() => {
     if (!UserContext) {
       setUserFetch([]);
+      return;
     }
+    const API = `https://api.github.com/search/users?q=${UserContext}`;
     setIsLoading(true);
     (async () => {
       try {
         console.log(API);
         const result = await fetch(API);
-
         const result2 = await result.json();
-        const fetchMap = result2.items.map((data) => data.login);
-        console.log(fetchMap);
-        setUserFetch(fetchMap);
+        if (result.ok) {
+          console.log("anything");
+          const fetchMap = result2.items.map((data) => data.login);
+          console.log(fetchMap);
+          setUserFetch(fetchMap);
+        } else {
+          console.log("else statement", result2);
+          setErrorMessage(result2.message);
+        }
       } catch (error) {
         throw error;
       } finally {
         setIsLoading(false);
       }
     })();
-  }, [API]);
+  }, [UserContext]);
 
   const user = userfetch.map((user) => {
-    console.log(user);
     return <li>{user}</li>;
   });
-  console.log(user);
 
   return (
     <div>
-      {errormessage ? errormessage : null}
+      {errormessage && <p>{errormessage}</p>}
       {isLoading && <p>loading..</p>}
-
       {user.length === 0 ? <p>No results</p> : user}
     </div>
   );
